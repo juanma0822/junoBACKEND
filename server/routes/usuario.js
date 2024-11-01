c80915f44b4063968e29ec693c6c6bf844003cd7
JuanCa-16 <juancamilohg162003@gmail.com>
2024-11-01 17:42:04 -0500

    Se crea ruta para el registro, editar perfil y clave

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -40,6 +40,21 @@ router.post('/', async (req, res) => {
     // Encriptar la contraseña
     const hashedPassword = await hashPassword(contraseña);
 
+     //2. Verificar que el Id y correo no existan
+    const user = await pool.query("SELECT * FROM Usuario where nombre_usuario = $1", [nombre_usuario]);
+
+    if(user.rows.length !== 0){
+        console.log('1')
+        return res.status(401).json({ error: 'Usuario ya registrado' });
+    }
+
+     const email = await pool.query("SELECT * FROM Usuario where correo_electronico = $1", [correo_electronico]);
+
+    if(email.rows.length !== 0){
+        console.log('2')
+        return res.status(401).json({ error: 'El correo ya se encuentra registrado' });
+    }
+
     // Consulta para insertar el nuevo usuario
     const query = `
       INSERT INTO Usuario (correo_electronico, nombre_usuario, nombre_real, apellidos, fecha_nacimiento, telefono, ciudad, contraseña, sexo, hora_alerta)
@@ -51,7 +66,11 @@ router.post('/', async (req, res) => {
 
     const result = await pool.query(query, values);
 
-    res.status(201).json({ message: 'Usuario creado exitosamente', usuario: result.rows[0] });
+    const usuario = result.rows[0];
+
+    const token = jwtGenerator(usuario.correo_electronico,usuario.nombre_usuario);
+
+    res.status(201).json({ message: 'Usuario creado exitosamente', usuario: usuario,llave: token });
   } catch (err) {
     console.error('Error al crear el usuario:', err);
     res.status(500).json({ error: 'Error al crear el usuario' });
@@ -81,6 +100,7 @@ router.get('/:correo_electronico', async (req, res) => {
     const result = await pool.query(query, [correo_electronico]);
 
     if (result.rows.length > 0) {
+      console.log((result.rows[0]));
       res.status(200).json(result.rows[0]);
     } else {
       res.status(404).json({ error: 'Usuario no encontrado' });
@@ -114,73 +134,93 @@ router.get('/correo/:nombre_usuario', async (req, res) => {
 // Ruta para actualizar un usuario por correo electrónico
 router.put('/:correo_electronico', async (req, res) => {
   const { correo_electronico } = req.params;
-  const { nombre_usuario, nombre_real, apellidos, fecha_nacimiento, telefono, ciudad, contraseña, sexo, hora_alerta } = req.body;
+  const { nombre_usuario, nombre_real, apellidos, fecha_nacimiento, telefono, ciudad, sexo } = req.body;
 
   try {
-    if (!nombre_usuario && !nombre_real && !apellidos && !fecha_nacimiento && !telefono && !ciudad && !contraseña && !sexo && !hora_alerta) {
-      return res.status(400).json({ error: 'No se proporcionaron datos para actualizar' });
-    }
+      // Verifica si hay datos para actualizar
+      if (!nombre_usuario && !nombre_real && !apellidos && !fecha_nacimiento && !telefono && !ciudad && !sexo) {
+          return res.status(400).json({ error: 'No se proporcionaron datos para actualizar' });
+      }
 
-    // Construir la consulta SQL dinámicamente
-    let query = 'UPDATE Usuario SET';
-    const values = [];
-    let i = 1;
+      // Verifica si el nombre de usuario ya existe para otro usuario (excluyendo el usuario actual)
+      const nomUsuario = await pool.query(
+          "SELECT * FROM Usuario WHERE nombre_usuario = $1 AND correo_electronico != $2", 
+          [nombre_usuario, correo_electronico]
+      );
 
-    if (nombre_usuario) {
-      query += ` nombre_usuario = $${i++},`;
-      values.push(nombre_usuario);
-    }
-    if (nombre_real) {
-      query += ` nombre_real = $${i++},`;
-      values.push(nombre_real);
-    }
-    if (apellidos) {
-      query += ` apellidos = $${i++},`;
-      values.push(apellidos);
-    }
-    if (fecha_nacimiento) {
-      query += ` fecha_nacimiento = $${i++},`;
-      values.push(fecha_nacimiento);
-    }
-    if (telefono) {
-      query += ` telefono = $${i++},`;
-      values.push(telefono);
-    }
-    if (ciudad) {
-      query += ` ciudad = $${i++},`;
-      values.push(ciudad);
-    }
-    if (contraseña) {
-      // Encriptar la contraseña antes de actualizar
-      const hashedPassword = await hashPassword(contraseña);
-      query += ` contraseña = $${i++},`;
-      values.push(hashedPassword);
-    }
-    if (sexo) {
-      query += ` sexo = $${i++},`;
-      values.push(sexo);
-    }
-    if (hora_alerta) {
-      query += ` hora_alerta = $${i++},`;
-      values.push(hora_alerta);
-    }
+      if (nomUsuario.rows.length > 0) {
+          return res.status(409).json({ error: 'Usuario ya en Uso' });
+      }
 
-    // Eliminar la última coma y agregar la condición WHERE
-    query = query.slice(0, -1) + ` WHERE correo_electronico = $${i}`;
-    values.push(correo_electronico);
+      // Realiza la actualización en la base de datos para la tabla Usuario
+      const query = `
+          UPDATE Usuario 
+          SET nombre_usuario = $1, nombre_real = $2, apellidos = $3, fecha_nacimiento = $4, telefono = $5, ciudad = $6, sexo = $7
+          WHERE correo_electronico = $8 
+          RETURNING *;
+      `;
+      const values = [nombre_usuario, nombre_real, apellidos, fecha_nacimiento, telefono, ciudad, sexo, correo_electronico];
+      const result = await pool.query(query, values);
 
-    const result = await pool.query(query, values);
+      // Si no se encuentra el usuario, devuelve un error 404
+      if (result.rows.length === 0) {
+          return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
 
-    if (result.rowCount > 0) {
-      res.status(200).json({ message: 'Usuario actualizado exitosamente' });
-    } else {
-      res.status(404).json({ error: 'Usuario no encontrado' });
-    }
+      // Obtener el usuario actualizado
+      const usuario = result.rows[0];
+
+      // Actualiza el nombre de usuario en las publicaciones
+      await pool.query(
+          "UPDATE publicacion SET username = $1 WHERE correo_usuario = $2", 
+          [nombre_usuario, correo_electronico]
+      );
+
+      // Genera el token
+      const token = jwtGenerator(usuario.correo_electronico, usuario.nombre_usuario);
+
+      // Devuelve la respuesta de éxito
+      res.status(200).json({ message: 'Datos Actualizados', usuario: usuario, llave: token });
+
   } catch (err) {
-    console.error('Error al actualizar el usuario:', err.message);
-    res.status(500).json({ error: 'Error al actualizar el usuario' });
+      console.error('Error al actualizar el usuario:', err.message);
+      res.status(500).json({ error: 'Error al actualizar el usuario' });
   }
 });
+
+
+// Ruta para actualizar clave de un usuario por correo electrónico
+router.put('/clave/:correo_electronico', async (req, res) => {
+  const { correo_electronico } = req.params;
+  const { contraseña } = req.body;
+
+  try {
+
+    // Encriptar la contraseña
+    const hashedPassword = await hashPassword(contraseña);
+
+    const query = `
+      UPDATE Usuario 
+      SET contraseña = $1 
+      WHERE correo_electronico = $2 
+      RETURNING *;
+    `;
+    const values = [hashedPassword,correo_electronico];
+    const result = await pool.query(query, values);
+
+
+    const usuario = result.rows[0];
+    const token = jwtGenerator(usuario.correo_electronico, usuario.nombre_usuario);
+
+    // Devuelve la respuesta de éxito
+    res.status(200).json({ message: 'Clave Actualizada ', usuario: usuario, llave: token });
+
+  } catch (err) {
+    console.error('Error al cambiar Clave:', err.message);
+    res.status(500).json({ error: 'Error al cambiar Clave' });
+  }
+});
+
 
 // Ruta para eliminar un usuario por correo electrónico
 router.delete('/:correo_electronico', async (req, res) => {
